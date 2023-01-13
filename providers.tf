89e2df28570673f1402dc0dda8d74e44b15cce7b
suhas2905 <117704560+suhas2905@users.noreply.github.com>
2023-01-13 05:04:51 -0500

    Update providers.tf

--- providers.tf
+++ providers.tf
@@ -21,3 +21,10 @@ provider "aws" {
   access_key = var.aws_access_key
   secret_key = var.aws_secret_key
 }
+
+provider "aws" {
+  alias  = "us-east-2"
+  region = "us-east-2"
+  access_key = var.aws_access_key
+  secret_key = var.aws_secret_key
+}
