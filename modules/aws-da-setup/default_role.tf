16363ad9a950bac9d637320e1dc0fec5d92b4925
suhas2905 <117704560+suhas2905@users.noreply.github.com>
2023-01-13 15:39:38 -0500

    Update default_role.tf

--- modules/aws-da-setup/default_role.tf
+++ modules/aws-da-setup/default_role.tf
@@ -17,85 +17,85 @@
  */
 
 locals {
-  instance_policy_name = "${lower(var.application_code)}-${lower(var.application_name)}-sap_hana_instance_default_policy-${lower(var.environment)}"
-  instance_role_name   = "${lower(var.application_code)}-${lower(var.application_name)}-sap_hana_instance_default_role-${lower(var.environment)}"
+  instance_policy_name = "${lower(var.application_code)}-${lower(var.application_name)}-sap_hana_instance_dr_default_policy-${lower(var.environment)}"
+  instance_role_name   = "${lower(var.application_code)}-${lower(var.application_name)}-sap_hana_instance_dr_default_role-${lower(var.environment)}"
 }
 
-#data "aws_iam_policy_document" "instance_trust" {
-#  statement {
-#    actions = ["sts:AssumeRole"]
-#    principals {
-#      type        = "Service"
-#      identifiers = ["ec2.amazonaws.com"]
-#    }
-#  }
-#}
+data "aws_iam_policy_document" "instance_trust_dr" {
+  statement {
+    actions = ["sts:AssumeRole"]
+    principals {
+      type        = "Service"
+      identifiers = ["ec2.amazonaws.com"]
+    }
+  }
+}
 
-#data "aws_iam_policy_document" "instance_policy" {
+data "aws_iam_policy_document" "instance_policy_dr" {
   # Statement for KMS handling
-#  statement {
-#    actions = [
-#      "kms:Encrypt",
-#      "kms:Decrypt",
-#      "kms:ReEncrypt*",
-#      "kms:GenerateDataKey*",
-#      "kms:DescribeKey"
-#    ]
-#    resources = [var.kms_key_arn]
-#  }
-#
+  statement {
+    actions = [
+      "kms:Encrypt",
+      "kms:Decrypt",
+      "kms:ReEncrypt*",
+      "kms:GenerateDataKey*",
+      "kms:DescribeKey"
+    ]
+    resources = [var.kms_key_arn]
+  }
+
   # Statement 1 for Stonith when HA installations
-#  statement {
-#    actions = [
-#      "ec2:DescribeInstances",
-#      "ec2:DescribeInstanceAttribute",
-#      "ec2:DescribeTags"
-#    ]
-#    resources = ["*"]
-#  }
+  statement {
+    actions = [
+      "ec2:DescribeInstances",
+      "ec2:DescribeInstanceAttribute",
+      "ec2:DescribeTags"
+    ]
+    resources = ["*"]
+  }
 
   # Statement 2 for Stonith when HA installations
- # statement {
- #   actions = [
- #     "ec2:ModifyInstanceAttribute",
- #     "ec2:RebootInstances",
- #     "ec2:StartInstances",
- #     "ec2:StopInstances"
- #   ]
- #   resources = ["arn:aws:ec2:*:*:instance/*"]
- # }
+  statement {
+    actions = [
+      "ec2:ModifyInstanceAttribute",
+      "ec2:RebootInstances",
+      "ec2:StartInstances",
+      "ec2:StopInstances"
+    ]
+    resources = ["arn:aws:ec2:*:*:instance/*"]
+  }
 
   # Statement 1 for Overlay IP when HA installations
-  #statement {
-  #  actions = [
-  #    "ec2:ReplaceRoute",
-  #    "ec2:DescribeRouteTables"
-  #  ]
-  #  resources = ["arn:aws:ec2:*:*:route-table/*"]
-  #}
+  statement {
+    actions = [
+      "ec2:ReplaceRoute",
+      "ec2:DescribeRouteTables"
+    ]
+    resources = ["arn:aws:ec2:*:*:route-table/*"]
+  }
 
   # Statement 2 for Overlay IP when HA installations
-  #statement {
-  #  actions = [
-  #    "ec2:ReplaceRoute",
-  #    "ec2:DescribeRouteTables"
-  #  ]
-  #  resources = ["*"]
-  #}
-#}
+  statement {
+    actions = [
+      "ec2:ReplaceRoute",
+      "ec2:DescribeRouteTables"
+    ]
+    resources = ["*"]
+  }
+}
 
-#module "default_instance_role" {
-#  source = "../_internal-modules/security/iam"
+module "default_instance_role" {
+  source = "../_internal-modules/security/iam"
 
-#  enabled = var.default_instance_role
+  enabled = var.default_instance_role
 
-#  policy_name = local.instance_policy_name
-#  policy      = data.aws_iam_policy_document.instance_policy.json
+  policy_name = local.instance_policy_name
+  policy      = data.aws_iam_policy_document.instance_policy_dr.json
 
-#  aws_managed_policies = ["arn:aws:iam::aws:policy/service-role/AmazonEC2RoleforSSM", "arn:aws:iam::aws:policy/AmazonSSMManagedInstanceCore", "arn:aws:iam::aws:policy/AmazonS3ReadOnlyAccess"]
-#  role_name            = local.instance_role_name
+  aws_managed_policies = ["arn:aws:iam::aws:policy/service-role/AmazonEC2RoleforSSM", "arn:aws:iam::aws:policy/AmazonSSMManagedInstanceCore", "arn:aws:iam::aws:policy/AmazonS3ReadOnlyAccess"]
+  role_name            = local.instance_role_name
 
-#  assume_role_policy = data.aws_iam_policy_document.instance_trust.json
+  assume_role_policy = data.aws_iam_policy_document.instance_trust_dr.json
 
-#  tags = merge(module.tags.values)
-#}
+  tags = merge(module.tags.values)
+}
