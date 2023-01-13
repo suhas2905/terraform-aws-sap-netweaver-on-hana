7b5e8e7fb3cac88552e72a082ba2f13f00b9d714
suhas2905 <117704560+suhas2905@users.noreply.github.com>
2023-01-13 14:20:53 -0500

    Update instance.tf

--- modules/_internal-modules_dr/compute/ec2-instance-linux/instance.tf
+++ modules/_internal-modules_dr/compute/ec2-instance-linux/instance.tf
@@ -20,7 +20,7 @@ resource "aws_instance" "linux-server" {
   ami                    = var.ami
   count                  = var.enabled == true ? var.instance_count : 0
   instance_type          = var.instance_type
-  subnet_id              = module.subnet.private_subnet[0].id
+  subnet_id              = module.subnets.private_subnet[0].id
   vpc_security_group_ids = var.vpc_security_group_ids
   user_data              = var.user_data != "" ? var.user_data : templatefile("${path.module}/default-user-data.sh", {})
   key_name               = var.ssh_key
