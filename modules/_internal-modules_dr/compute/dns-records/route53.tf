a6fd93b3aa54128186d885617ea169c7c3982a7d
suhas2905 <117704560+suhas2905@users.noreply.github.com>
2023-01-16 10:33:00 -0500

    Update route53.tf

--- modules/_internal-modules_dr/compute/dns-records/route53.tf
+++ modules/_internal-modules_dr/compute/dns-records/route53.tf
@@ -19,7 +19,7 @@
 resource "aws_route53_record" "record" {
   count   = length(var.record_names)
   zone_id = var.zone_id
-  name    = dr_var.record_names[count.index]
+  name    = var.record_names[count.index]_dr
   type    = var.record_type
   ttl     = var.record_ttl
   records = [var.records[count.index]]
