c75255216dc36b57336dbc56fa209805ded220d4
suhas2905 <117704560+suhas2905@users.noreply.github.com>
2023-01-17 08:59:19 -0500

    Update main.tf

--- modules/_internal-modules/storage/efs/main.tf
+++ modules/_internal-modules/storage/efs/main.tf
@@ -34,7 +34,8 @@ resource "aws_efs_mount_target" "main" {
   ]
 }
 resource "aws_efs_replication_configuration" "main" {
-  source_file_system_id = aws_efs_file_system.main[0].id
+  count      = var.enabled ? 1 : 0
+  source_file_system_id = aws_efs_file_system.main.*.id[0]
 
   destination {
     region = "us-west-2"
