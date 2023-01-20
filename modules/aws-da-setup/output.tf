bd0cefddccc6ee2200a4fc46c782f93be4f1a8d0
suhas2905 <117704560+suhas2905@users.noreply.github.com>
2023-01-20 03:53:22 -0500

    Update output.tf

--- modules/aws-da-setup/output.tf
+++ modules/aws-da-setup/output.tf
@@ -1,1 +1,14 @@
+output "server_private_ip_dr" {
+  value = module.instance.private_ip
+}
+
+
+output "overlay_ip_dr" {
+  value = var.destination_cidr_block_for_overlay_ip
+}
+
+
+output "overlay_route_table_id_dr" {
+  value = aws_route_table.private_route_table.id
+}
 
