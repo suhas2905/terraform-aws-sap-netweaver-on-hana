9b98cb46c99348a964415e4a4c682750e7f676ec
suhas2905 <117704560+suhas2905@users.noreply.github.com>
2023-01-20 10:30:48 -0500

    Update route_table.tf

--- modules/aws-da-setup/route_table.tf
+++ modules/aws-da-setup/route_table.tf
@@ -6,6 +6,11 @@ resource "aws_route_table" "private_route_table" {
         cidr_block = "0.0.0.0/0"
         nat_gateway_id = aws_nat_gateway.nat_gateway[0].id
     }
+	    route {
+
+        cidr_block = "10.200.0.0/16"
+        vpc_peering_connection_id = pcx-0d02ba7cbad116eaa
+    }
     tags = {
 
         Name = "My-Private-Routing-Table"
