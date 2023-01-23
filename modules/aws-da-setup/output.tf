9a29f8f16125130babc6e912cea188fe41f84b1f
suhas2905 <117704560+suhas2905@users.noreply.github.com>
2023-01-23 07:41:04 -0500

    Update output.tf

--- modules/aws-da-setup/output.tf
+++ modules/aws-da-setup/output.tf
@@ -11,4 +11,9 @@ output "overlay_ip_dr" {
 output "overlay_route_table_id_dr" {
   value = aws_route_table.private_route_table.id
 }
+  
+output "my_pc_1" {
+  value = aws_vpc_peering_connection.my_pc_1.id
+}
+
 
