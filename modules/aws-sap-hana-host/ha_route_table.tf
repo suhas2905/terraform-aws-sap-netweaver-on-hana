d56170bc58cae024104f5032f29c124a1b55439d
suhas2905 <117704560+suhas2905@users.noreply.github.com>
2023-01-23 09:04:53 -0500

    Update ha_route_table.tf

--- modules/aws-sap-hana-host/ha_route_table.tf
+++ modules/aws-sap-hana-host/ha_route_table.tf
@@ -33,5 +33,5 @@ resource "aws_route" "ha_route_1" {
 
   route_table_id         = data.aws_route_table.ha_route_table.id
   destination_cidr_block = var.vpc_cidr_dr
-  vpc_peering_connection_id = modules.aws-da-setup.my_pc_1
+  vpc_peering_connection_id = modules.da_hana_host.my_pc_1
 }
