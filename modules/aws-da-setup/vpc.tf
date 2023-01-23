d5dab05bbb56cce2b5deae390c817473955991e8
suhas2905 <117704560+suhas2905@users.noreply.github.com>
2023-01-23 07:26:47 -0500

    Update vpc.tf

--- modules/aws-da-setup/vpc.tf
+++ modules/aws-da-setup/vpc.tf
@@ -1,7 +1,10 @@
 resource "aws_vpc_peering_connection" "my_pc_1" {
   peer_vpc_id   = var.vpc_id
   vpc_id        = aws_vpc.SAP_VPC[0].id
-  peer_region   = "us-east-2"
+  peer_region   = "us-east-1"
+  tags = {
+        Name = "my_pc_1_owner"
+    }
 }
 resource "aws_vpc" "SAP_VPC" {
     count                = 1
