c95a070a3554d2bbcce9b80acf1582b3929c708d
suhas2905 <117704560+suhas2905@users.noreply.github.com>
2023-01-27 12:40:43 -0500

    Update subnets.tf

--- modules/aws-da-setup/subnets.tf
+++ modules/aws-da-setup/subnets.tf
@@ -1,7 +1,7 @@
 resource "aws_subnet" "private_subnet" {
     count                   = length(var.subnets_cidr) 
     vpc_id                  = aws_vpc.SAP_VPC[0].id
-    cidr_block              = element(var.subnets_cidr , count.index) 
+    cidr_block              = element(var.private_subnets_cidr , count.index) 
     availability_zone       = element(var.availability_zones , count.index)
     map_public_ip_on_launch = false
 
