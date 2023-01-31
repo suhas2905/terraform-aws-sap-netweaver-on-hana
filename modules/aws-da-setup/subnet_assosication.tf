b73eb1c41ce225ade4abc360003c4f63e6a8c9f7
suhas2905 <117704560+suhas2905@users.noreply.github.com>
2023-01-31 10:26:36 -0500

    Update subnet_assosication.tf

--- modules/aws-da-setup/subnet_assosication.tf
+++ modules/aws-da-setup/subnet_assosication.tf
@@ -1,13 +1,13 @@
 resource "aws_route_table_association" "private_sub" {
 
-    count          = length(var.subnets_cidr)
+    count          = length(var.private_subnets_cidr)
     subnet_id      = element(aws_subnet.private_subnet.*.id , count.index)
     route_table_id = aws_route_table.private_route_table.id
 
 }
 resource "aws_route_table_association" "public_sub" {
 
-    count          = length(var.subnets_cidr)
+    count          = length(var.public_subnets_cidr)
     subnet_id      = element(aws_subnet.public_subnet.*.id , count.index)
     route_table_id = aws_route_table.public_route_table.id
 
