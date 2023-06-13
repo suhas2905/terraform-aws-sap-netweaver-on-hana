0b131c68ca4e2e19fb2f0fe28ca9c31b0ff7774d
suhas2905 <117704560+suhas2905@users.noreply.github.com>
2023-06-13 20:39:45 +0530

    Update vpc.tf

--- modules/aws-da-setup/vpc.tf
+++ modules/aws-da-setup/vpc.tf
@@ -4,7 +4,7 @@ resource "aws_vpc" "SAP_VPC" {
     cidr_block           = var.vpc_cidr
     enable_dns_support   = "true" #gives you an internal domain name
     enable_dns_hostnames = "true" #gives you an internal host name
-    enable_classiclink   = "false"
+#    enable_classiclink   = "false"
     instance_tenancy     = "default"    
     
     tags = {
